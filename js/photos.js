589c821433636b2c082e80b50bb4a3faa695a7bb
George191 <67489648@qq.com>
2019-08-07 22:04:35 +0800

    Site updated: 2019-08-07 22:04:35

--- js/photos.js
+++ js/photos.js
@@ -1,50 +1,19 @@
-/**
- * Created by George on 2019/8/7.
- */
-define([], function () {
-  return {
-    page: 1,
-    offset: 20,
-    init: function () {
-      var that = this;
-      $.getJSON("source/lock/data/data.json", function (data) {
+init: function () {
+    var that = this;
+    $.getJSON("/photos/data.json", function (data) {
         that.render(that.page, data);
-
         that.scroll(data);
-      });
-    },
-
-    render: function (page, data) {
-      var begin = (page - 1) * this.offset;
-      var end = page * this.offset;
-      if (begin >= data.length) return;
-      var html, li = "";
-      for (var i = begin; i < end && i < data.length; i++) {
-        li += '<li><div class="img-box">' +
-          '<a class="img-bg" rel="example_group" href="https://oc1gyfe6q.qnssl.com/' + data[i] + '?raw=true"></a>' +
-          '![](https://oc1gyfe6q.qnssl.com/' + data[i] + '?raw=true)' +
-          '</li>';
-      }
-
-
-      $(".img-box-ul").append(li);
-      $(".img-box-ul").lazyload();
-      $("a[rel=example_group]").fancybox();
-    },
-
-    scroll: function (data) {
-      var that = this;
-      $(window).scroll(function () {
-        var windowPageYOffset = window.pageYOffset;
-        var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
-        var sensitivity = 0;
-
-        var offsetTop = $(".instagram").offset().top + $(".instagram").height();
-
-        if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
-          that.render(++that.page, data);
-        }
-      })
+    });
+},
+render: function (page, data) {
+    var begin = (page - 1) * this.offset;
+    var end = page * this.offset;
+    if (begin >= data.length) return;
+    var html, li = "";
+    for (var i = begin; i < end && i < data.length; i++) {
+        li += '<li><div class="img-box">' + '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/George191/blog_Album/master/min_photos/' + data[i] + '"></a>' + '<img lazy-src="https://raw.githubusercontent.com/George191/blog_Album/master/min_photos/' + data[i] + '" />' + '</li>';
     }
-  }
-})
+    $(".img-box-ul").append(li);
+    $(".img-box-ul").lazyload();
+    $("a[rel=example_group]").fancybox();
+},
